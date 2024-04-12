655381b8ca8957fb1d7530ee6aa411ec6eb81410
Atziri Camacho <atziricamher@gmail.com>
2024-04-12 16:29:22 -0600

    script a login y derivados

--- script.js
+++ script.js
@@ -5,27 +5,58 @@ function login() {
     let password = document.getElementById("contraseña").value;
 
     if(user == "" || password == ""){
-        alert("No se admiten espacios en blanco");
+        swal('¡Ha ocurrido un error!', 'No se admiten espacios en blanco', 'error');  /* Libreria para modificar el diseño de la alerta */
     } else {
-        alert(`Bienvenido/a ${user}`);
         window.location="index.html";
     }
 }
 
-/* --------------------------------------------------------------------------------- Libros > Carrito --------------------- */
+function recuperarContraseña(){
+    let correo = document.getElementById("correo").value;
+    let verificacion = document.getElementById("verificacion").value;
 
-function viaje(){
-    let casaDelPeligro = 149;
-    console.log(casaDelPeligro);
-};
-
-function idk(){
-    let idk = 149;
-    console.log(idk);
-};
-
-/* --------------------------------------------------------------------------------- Libros > Carrito --------------------- */
-
-function compra(){
-    window.location="compra.html";
+    if(correo == "" || verificacion == ""){
+        swal('¡Ha ocurrido un error!', 'Campos necesarios de verificación', 'error');  /* Libreria para modificar el diseño de la alerta */
+    } else {
+        window.location="loginhtml";
+    }
 }
+
+function crearCuenta() {
+    let nombre = document.getElementById("nombre").value;
+    let numero = document.getElementById("numero").value;
+    let contraseña = document.getElementById("contraseña").value;
+    let email = document.getElementById("email").value;
+    let domicilio = document.getElementById("domicilio").value;
+    let confirmacion = document.getElementById("confirmacion").value;
+
+    if(nombre == "" || numero == "" || contraseña == "" || email == "" || domicilio == "" || confirmacion == ""){
+        swal('¡Ha ocurrido un error!', 'Rellena los campos indicados', 'error');  /* Libreria para modificar el diseño de la alerta */
+    } else {
+        window.location="login.html";
+    }
+}
+
+/* --------------------------------------------------------------------------------- Libros > Carrito --------------------- */
+function compra(){
+    let num1 = document.getElementById("1");
+    let num2 = document.getElementById("2");
+
+    num1.addEventListener("click", acumulador);
+    num2.addEventListener("click", acumulador);
+    
+    let n1 = 149;
+    let n2 = 120;
+
+    function acumulador(){
+        let acumulador = 0;
+
+        acumulador = acumulador + n1;
+        acumulador = acumulador + n2;
+
+        console.log(acumulador);
+    }
+    
+    acumulador();
+}
+
