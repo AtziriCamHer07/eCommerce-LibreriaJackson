4bbbad6e8668a5df08ef3ab5615b90f08d3d1025
Atziri Camacho <atziricamher@gmail.com>
2024-04-12 22:05:16 -0600

    rebajas.html

--- script.js
+++ script.js
@@ -76,3 +76,78 @@ function compra(){
     acumulador();
 }
 
+/* ---------------------------------------------------------------------------------------------------------------------------- Countdown --------------------- */
+
+let horas = 18;
+let minutos = 34;
+let segundos = 48;
+
+//definir y ejecutar los segundos
+function cargarSegundo(){
+    let txtSegundos;
+
+    if(segundos < 0){
+        segundos = 59;
+    }
+
+    if(segundos < 10){
+        txtSegundos = `0${segundos}`;
+    } else {
+        txtSegundos = segundos;
+    }
+
+    document.getElementById('segundos').innerHTML = txtSegundos;
+    segundos--;
+
+    cargarMinutos(segundos);
+}
+
+//definir y ejecutar los minutos
+
+function cargarMinutos(segundos){
+    let txtMinutos;
+
+    if(segundos == -1 && minutos !== 0){
+        setTimeout(() =>{
+            minutos = 59;
+        },500);
+    }
+
+    //mostrar minutos en pantalla
+    if(minutos < 10){
+        txtMinutos = `0${minutos}`;
+    } else {
+        txtMinutos = minutos;
+    }
+
+    document.getElementById('minutos').innerHTML = txtMinutos;
+
+    cargarHoras(segundos, minutos);
+}
+
+//definir y ejecutar las horas
+
+function cargarHoras(segundos, minutos){
+    let txtHoras;
+
+    if(segundos == -1 && minutos == 0 && horas !== 0){
+        setTimeout(() =>{
+            horas--;
+        },500);
+    } else if(segundos == -1 && minutos == 0 && horas == 0){
+        setTimeout(() =>{
+            horas = 2;
+        },500);
+    }
+
+    //mostrar horas en pantalla
+    if(horas < 10){
+        txtHoras = `0${horas}`;
+    } else {
+        txtHoras = horas;
+    }
+
+    document.getElementById('horas').innerHTML = txtHoras;
+}
+
+setInterval(cargarSegundo,1000);
